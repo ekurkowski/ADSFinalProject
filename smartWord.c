9857d382ec47d631d723adfe0473baddf2313112
bethkurkowski <ekurkowski2016@my.fit.edu>
2018-11-25 18:32:58 -0500

    comments for austin

--- smartWord.c
+++ smartWord.c
@@ -16,10 +16,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #include "smartWord.h"
 
-#include <ctype.h>
+
 
 struct hash{
 	struct inter *range[26];
@@ -32,13 +33,13 @@ struct inter{
 
 struct node{
 	char pos_word[MAX_WORDLEN];
-	int score; // number of times used 
+	int score; // number of times used
 	struct node *next;
 };
 
 void create_space();
-void initSmartWord(char word[100]);
-void procOldMsgSmartWord(char word[100]);
+void initSmartWord(FILE *wordFile);
+void procOldMsgSmartWord(FILE *oldWords);
 int find_word(char word[100],int index,int index_2);
 
 void create_space(){
@@ -74,43 +75,50 @@ void insert(char word[100],int index,int index_2){
 	return;
 }
 
-void initSmartWord(char word[100]){
-	int length = strlen(word);
-	int f_c_value,s_c_value;  //first_character_value and second
-	for(int i=0;i<length;i++){  //sets word to lowercase
-		word[i] = tolower(word[i]);
-	}
+void initSmartWord(FILE *wordFile){
+  create_space();//functions mallocs all possible indexs in hashtables
+	char word[100];
+	while(fscanf(wordFile,"%s",word)==1){ //reads entire words.txt file
+  	int length = strlen(word);
+  	int f_c_value,s_c_value;  //first_character_value and second
+  	for(int i=0;i<length;i++){  //sets word to lowercase
+  		word[i] = tolower(word[i]);
+  	}
 
-	f_c_value = word[0];  //gets ascii numerical value
-	s_c_value = word[1];
+  	f_c_value = word[0];  //gets ascii numerical value
+  	s_c_value = word[1];
 
-	f_c_value = f_c_value%97; //mods number by 97 to get specific index to put word at in hash tables
-	s_c_value = s_c_value%97;
-	insert(word,f_c_value,s_c_value);
+  	f_c_value = f_c_value%97; //mods number by 97 to get specific index to put word at in hash tables
+  	s_c_value = s_c_value%97;
+  	insert(word,f_c_value,s_c_value);
+  }
 
 	return;
 }
 
-void procOldMsgSmartWord(char word[100]){
-	int length = strlen(word);
-	int f_c_value,s_c_value; //first_character_value and second
-	int found_ctr;
-	for(int i=0;i<length;i++){  //sets word to lowercase
-		word[i] = tolower(word[i]);
-	}
+void procOldMsgSmartWord(FILE *wordFile){
+  char word[100];
+  while(fscanf(wordFile,"%s",word)==1){  //reads entire old_tweets files
+  	int length = strlen(word);
+  	int f_c_value,s_c_value; //first_character_value and second
+  	int found_ctr;
+  	for(int i=0;i<length;i++){  //sets word to lowercase
+  		word[i] = tolower(word[i]);
+  	}
 
-	f_c_value = word[0];  //gets ascii numerical value
-	s_c_value = word[1];
+  	f_c_value = word[0];  //gets ascii numerical value
+  	s_c_value = word[1];
 
-	f_c_value = f_c_value%97; //mods number by 97 to get specific index to put word at in hash tables
-	s_c_value = s_c_value%97;
+  	f_c_value = f_c_value%97; //mods number by 97 to get specific index to put word at in hash tables
+  	s_c_value = s_c_value%97;
 
-	found_ctr = find_word(word,f_c_value,s_c_value);
+  	found_ctr = find_word(word,f_c_value,s_c_value);
 
-	if(found_ctr == 0){ //if word was not found it will insert it into the structure
-		insert(word,f_c_value,s_c_value);
-	}
-
+  	if(found_ctr == 0){ //if word was not found it will insert it into the structure
+  		insert(word,f_c_value,s_c_value);
+  	}
+  }
+// https://www.programiz.com/c-programming/examples/remove-characters-string
 	return ;
 }
 
@@ -151,37 +159,10 @@ Word *PrevGuessed = NULL;
  */
 
 
-
-// initialize smartWord by process a file of English dictionary words
-void initSmartWord(char *wordFile)
-{
- // can we normalize everything to be lowercase before we put it in the database?
-  /*
-    description of declared variables
-   */
-
-
-
-  /*
-    description for each "block" (about 5 lines of instructions)
-   */
-
-}
-
-
-// process old messages from oldMessageFile
-void procOldMsgSmartWord(char *oldMessageFile)
-{
-
-   // can we normalize everything to be lowercase before we put it in the database?
-   // useful link to make a new function that removes everything but letters
-   // https://www.programiz.com/c-programming/examples/remove-characters-string
-}
-
-
 void GetHighestScores(Word *one, Word *two, Word *three, Word *four) {
   // return the highest three scores out of the four
   // this might not be super efficient and could use some help I think
+  // 3 words already in list then fourth is checking the lowest ranked of the three guesses if it is higher check if it is also higher then the second if it is check if it is higher than the first, whenever it finds one that it's lower than stop and replace except if it is lower than the lowest one then return null
 }
 
 
@@ -258,7 +239,6 @@ void guessSmartWord(char letter, int letterPosition, int wordPosition,char guess
 
 // values for bool: true (1), false (0)
 
-void feedbackSmartWord(bool isCorrectGuess, char *correctWord)
-{
+void feedbackSmartWord(bool isCorrectGuess, char *correctWord) {
 
 }
